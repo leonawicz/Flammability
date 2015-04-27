7da3709211a4fa74e8c7d3c5ffc9115d2229d2f8
Matt Leonawicz <matt.rstat@gmail.com>
2015-04-27 17:43:07 -0600

    Generalize to means- vs. samples-based

--- code/duplicate_flam_maps.R
+++ code/duplicate_flam_maps.R
@@ -4,7 +4,7 @@
 
 #### Script author:  Matthew Leonawicz ####
 #### Maintainted by: Matthew Leonawicz ####
-#### Last updated:   04/08/2015        ####
+#### Last updated:   04/27/2015        ####
 
 # @knitr duplicate
 comargs <- (commandArgs(TRUE))
@@ -14,14 +14,16 @@ if(!exists("period")) stop("Argument 'period' not passed at command line.")
 if(!exists("model")) stop("Argument 'model' not passed at command line.")
 if(!(period %in% c("historical", "rcp45", "rcp60", "rcp85"))) stop("Invalid period specified.")
 if(!(model %in% c("CRU31", "CCSM4", "GFDL-CM3", "GISS-E2-R", "IPSL-CM5A-LR", "MRI-CGCM3"))) stop("Invalid data set specified.")
+if(!exists("samples")) samples <- TRUE
 if(!exists("mapset")) stop("Argument 'mapset' not passed at command line.") # Currently must be "3models_tif" or "5models_tif"
 if(!exists("out")) stop("Argument 'out' not passed at command line.")
 if(!exists("n")) n <- 40
 if(!exists("yearloop")) stop("Argument 'yearloop' not passed at command line.")
 
 library(parallel)
-mainDir <- file.path("/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/data/gbmFlammability", period, model, mapset)
-dir.create(outDir <- file.path("/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/data/gbmFlammability", period, model, out), showWarnings=FALSE)
+verDir <- if(samples) "samples_based" else "means_based"
+mainDir <- file.path("/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/data/gbmFlammability", verDir, period, model, mapset)
+dir.create(outDir <- file.path("/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/data/gbmFlammability", verDir, period, model, out), showWarnings=FALSE)
 setwd(outDir)
 
 files <- list.files(pattern="\\.tif$", full=TRUE)
