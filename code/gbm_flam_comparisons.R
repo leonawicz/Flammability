a83ffa8ad3b8ed7edb52408b8101035cb340cb74
Matt Leonawicz <matt.rstat@gmail.com>
2015-06-03 19:21:45 -0600

    Add threshold processing and maps

--- code/gbm_flam_comparisons.R
+++ code/gbm_flam_comparisons.R
@@ -4,9 +4,9 @@
 
 #### Script author:  Matthew Leonawicz ####
 #### Maintainted by: Matthew Leonawicz ####
-#### Last updated:   05/28/2015        ####
+#### Last updated:   06/01/2015        ####
 
-# @knitr setup
+# @knitr setup1
 setwd("/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/data/gbmFlammability/samples_based/historical/CRU31")
 dir.create(plotDir <- "/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/plots/gbmFlammability/map_comparisons", showWarnings=FALSE)
 
@@ -30,8 +30,57 @@ r2b <- raster(list.files(dirs[5], pattern="1917.tif", full=T))
 s2 <- stack(r1b, r2b, r1b-r2b)
 names(s2) <- c("GBM3", "GBM5", "Difference")
 
+# @knitr minXY
+# Minimum threshold analysis
+# RV's X = flammability and Y = lightning probability, with condition min(XY) = c
+# @knitr setup1
+setwd("/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/data/gbmFlammability/samples_based/historical/CRU31")
+dir.create(plotDir <- "/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/plots/gbmFlammability/map_comparisons", showWarnings=FALSE)
+
+library(rasterVis)
+(dirs <- list.files())
+dirs3 <- dirs[4]
+dirs5 <- dirs[8]
+files3 <- unlist(lapply(dirs3, function(x, year) list.files(x, pattern=paste0("\\.tif$"), full=TRUE)))
+files5 <- unlist(lapply(dirs5, function(x, year) list.files(x, pattern=paste0("\\.tif$"), full=TRUE)))
+
+threshold <- 0.05
+#default.pvalue <- 0.25
+s3 <- stack(files3)
+s5 <- stack(files5)
+qtile3 <- qtile5 <- pval3 <- pval5 <- rep(NA, nlayers(s3))
+for(i in 1:nlayers(s3)){
+	r3 <- subset(s3, i)
+	r5 <- subset(s5, i)
+    #qtile3[i] <- as.numeric(quantile(r3[], prob=default.pvalue, na.rm=T))
+    #qtile5[i] <- as.numeric(quantile(r5[], prob=default.pvalue, na.rm=T))
+	pval3[i] <- mean(r3[] <= threshold, na.rm=T)
+	pval5[i] <- mean(r5[] <= threshold, na.rm=T)
+	r3[r3 <= threshold] <- threshold
+	r5[r5 <= threshold] <- threshold
+	r3[r3 > threshold] <- 0
+	r5[r5 > threshold] <- 0
+	r3[r3 == threshold] <- 1
+	r5[r5 == threshold] <- 1
+	if(i==1) {
+		r3tot <- r3 ; r5tot <- r5
+	} else {
+		r3tot <- r3tot + r3
+		r5tot <- r5tot + r5
+	}
+	print(nlayers(s3) - i)
+}
+
+r3tot <- r3tot/nlayers(s3)
+r5tot <- r5tot/nlayers(s5)
+s <- stack(r3tot, r5tot)
+names(s) <- c("GBM-3", "GBM-5")
+
 # @knitr plot
 # Setup
+library(grid)
+library("gridBase")
+
 at.vals <- c(0, 0.25, 0.499, 0.75, 1)
 colkey <- list(at=at.vals, labels=list(labels=c("Low", "Medium", "High", "Severe"), at=at.vals + 0.125))
 
@@ -53,3 +102,30 @@ png(paste0(plotDir, "/GBM3vsGBM5_1977.png"), height=800, width=2400, res=200)
 p <- levelplot(s2, maxpixels=ncell(s)/10, main=paste("1977 3- vs. 5-GBM flammability maps"), par.settings=revRasterTheme, contour=T, margin=F)#, at=at.vals, colorkey=colkey)
 print(p)
 dev.off()
+
+png(paste0(plotDir, "/flam_spacetime_threshDist_", threshold, ".png"), height=2400, width=2400, res=200)
+p <- levelplot(s, maxpixels=ncell(s)/10, main=paste("1950-2009 0.175 flammability threshold distributions"), par.settings=revRasterTheme, contour=T, margin=F)#, at=at.vals, colorkey=colkey)
+grid.newpage()
+pushViewport(viewport(layout=grid.layout(2, 2, widths=unit(c(1, 1), "null"), heights=unit(c(1, 1), "null"))))
+
+vp <- pushViewport(viewport(layout.pos.row=1, layout.pos.col=1))
+par(omi=gridOMI())
+# base graphics
+hist(pval3, main=paste("GBM-3 % area <=", threshold), col="gray", xlab="", ylab="", freq=FALSE)
+popViewport()
+
+vp <- pushViewport(viewport(layout.pos.row=1, layout.pos.col=2))
+par(omi=gridOMI())
+par(new=TRUE)
+# base graphics
+hist(pval5, main=paste("GBM-5 % area <=", threshold), col="gray", xlab="", ylab="", freq=FALSE)
+popViewport()
+
+# lattice plot
+vp <- pushViewport(viewport(layout.pos.row=2, layout.pos.col=1:2))
+print(p, vp=vp, newpage=FALSE)
+popViewport()
+
+popViewport()
+dev.off()
+
