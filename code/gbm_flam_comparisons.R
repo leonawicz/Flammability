2c8d875a5ae7461cd14e624ad0372c9bee77861b
Matt Leonawicz <matt.rstat@gmail.com>
2015-06-05 12:46:46 -0600

    Add plots, include Noatak domain, general cleanup

--- code/gbm_flam_comparisons.R
+++ code/gbm_flam_comparisons.R
@@ -4,33 +4,40 @@
 
 #### Script author:  Matthew Leonawicz ####
 #### Maintainted by: Matthew Leonawicz ####
-#### Last updated:   06/01/2015        ####
+#### Last updated:   06/05/2015        ####
 
 # @knitr setup1
+# GBM flammability map comparisons
 setwd("/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/data/gbmFlammability/samples_based/historical/CRU31")
 dir.create(plotDir <- "/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/plots/gbmFlammability/map_comparisons", showWarnings=FALSE)
 
 library(rasterVis)
 (dirs <- list.files())
-dirs3 <- dirs[1:3]
-dirs5 <- dirs[5:7]
+dirs3 <- dirs[c(1,3)]
+dirs5 <- dirs[c(5,7)]
 
-yr <- 2004
+noa.shp <- shapefile("/big_scratch/mfleonawicz/Alf_Files_20121129/noa_basin2/Noa_basin2")
+
+yr <- 1957
 files3 <- unlist(lapply(dirs3, function(x, year) list.files(x, pattern=paste0("_", year, "\\.tif$"), full=TRUE), year=yr))
-files5 <- unlist(lapply(dirs3, function(x, year) list.files(x, pattern=paste0("_", year, "\\.tif$"), full=TRUE), year=yr))
+files5 <- unlist(lapply(dirs5, function(x, year) list.files(x, pattern=paste0("_", year, "\\.tif$"), full=TRUE), year=yr))
 s <- stack(c(files3, files5))
-names(s) <- paste0(rep(c("GBM3", "GBM5"), each=length(dirs3)), paste0("_Flammability", c("", "_Lightning0.5", "0.5_Lightning 0.5")))
+names(s) <- paste0(rep(c("GBM3", "GBM5"), each=length(dirs3)), paste0("_Flammability", c("", "0.01_Lightning_0.75")))
 s <- brick(s)
 
 # Check for difference between GBM 3-model and GBM 5-model versions in CAVM region
-r1 <- raster(list.files(dirs[4], pattern="1977.tif", full=T))
-r2 <- raster(list.files(dirs[8], pattern="1977.tif", full=T))
-r1b <- raster(list.files(dirs[1], pattern="1917.tif", full=T))
-r2b <- raster(list.files(dirs[5], pattern="1917.tif", full=T))
+pat <- paste0(yr, ".tif")
+r1 <- raster(list.files(dirs[4], pattern=pat, full=T))
+r2 <- raster(list.files(dirs[8], pattern=pat, full=T))
+r1b <- raster(list.files(dirs[1], pattern=pat, full=T))
+r2b <- raster(list.files(dirs[5], pattern=pat, full=T))
 s2 <- stack(r1b, r2b, r1b-r2b)
 names(s2) <- c("GBM3", "GBM5", "Difference")
 
-# @knitr minXY
+s.noa <- mask(crop(s, noa.shp), noa.shp)
+s2.noa <- mask(crop(s2, noa.shp), noa.shp)
+
+# @knitr setup2
 # Minimum threshold analysis
 # RV's X = flammability and Y = lightning probability, with condition min(XY) = c
 # @knitr setup1
@@ -41,13 +48,23 @@ library(rasterVis)
 (dirs <- list.files())
 dirs3 <- dirs[4]
 dirs5 <- dirs[8]
-files3 <- unlist(lapply(dirs3, function(x, year) list.files(x, pattern=paste0("\\.tif$"), full=TRUE)))
-files5 <- unlist(lapply(dirs5, function(x, year) list.files(x, pattern=paste0("\\.tif$"), full=TRUE)))
+f <- function(x) list.files(x, pattern=paste0("\\.tif$"), full=TRUE)
+files3 <- unlist(lapply(dirs3, f))
+files5 <- unlist(lapply(dirs5, f))
 
-threshold <- 0.05
+noa.shp <- shapefile("/big_scratch/mfleonawicz/Alf_Files_20121129/noa_basin2/Noa_basin2")
+
+threshold <- 0.01
 #default.pvalue <- 0.25
 s3 <- stack(files3)
 s5 <- stack(files5)
+domain <- "Noatak" # "Statewide"
+if(domain=="Noatak"){
+    s3 <- crop(s3, noa.shp)
+    s5 <- crop(s5, noa.shp)
+    s3 <- mask(s3, noa.shp)
+    s5 <- mask(s5, noa.shp)
+}
 qtile3 <- qtile5 <- pval3 <- pval5 <- rep(NA, nlayers(s3))
 for(i in 1:nlayers(s3)){
 	r3 <- subset(s3, i)
@@ -92,19 +109,35 @@ revRasterTheme <- function (pch = 19, cex = 0.7, region=colorRampPalette(brewer.
     theme
 }
 
-# Write PNGs
-png(paste0(plotDir, "/gbm.flamm_", yr,"_comparisons.png"), height=1600, width=2400, res=200)
-p <- levelplot(s, maxpixels=ncell(s)/10, main=paste(yr,"flammability map comparisons"), par.settings=revRasterTheme, contour=T, margin=F)#, at=at.vals, colorkey=colkey)
+
+# Write PNGs from part one: comparisons
+# Statewide
+w <- h <- 1600
+png(paste0(plotDir, "/gbm.flamm_", yr,"_comparisons_Statewide.png"), height=h, width=w, res=200)
+p <- levelplot(s, maxpixels=ncell(s)/10, main=paste(yr, "flammability map comparisons"), par.settings=revRasterTheme, contour=T, margin=F)#, at=at.vals, colorkey=colkey)
 print(p)
 dev.off()
 
-png(paste0(plotDir, "/GBM3vsGBM5_1977.png"), height=800, width=2400, res=200)
-p <- levelplot(s2, maxpixels=ncell(s)/10, main=paste("1977 3- vs. 5-GBM flammability maps"), par.settings=revRasterTheme, contour=T, margin=F)#, at=at.vals, colorkey=colkey)
+png(paste0(plotDir, "/GBM3vsGBM5_", yr, "_Statewide.png"), height=h, width=w*1.5, res=200)
+p <- levelplot(s2, maxpixels=ncell(s)/10, main=paste(yr, "3- vs. 5-GBM flammability maps"), par.settings=revRasterTheme, contour=T, margin=F)#, at=at.vals, colorkey=colkey)
 print(p)
 dev.off()
 
-png(paste0(plotDir, "/flam_spacetime_threshDist_", threshold, ".png"), height=2400, width=2400, res=200)
-p <- levelplot(s, maxpixels=ncell(s)/10, main=paste("1950-2009 0.175 flammability threshold distributions"), par.settings=revRasterTheme, contour=T, margin=F)#, at=at.vals, colorkey=colkey)
+# Noatak
+png(paste0(plotDir, "/gbm.flamm_", yr,"_comparisons_Noatak.png"), height=h, width=w*1.5, res=200)
+p <- levelplot(s.noa, maxpixels=ncell(s.noa), main=paste(yr,"flammability map comparisons"), par.settings=revRasterTheme, contour=T, margin=F)#, at=at.vals, colorkey=colkey)
+print(p)
+dev.off()
+
+png(paste0(plotDir, "/GBM3vsGBM5_", yr, "_Noatak.png"), height=h*1.5, width=w, res=200)
+p <- levelplot(s2.noa, maxpixels=ncell(s2.noa), main=paste(yr, "3- vs. 5-GBM flammability maps"), par.settings=revRasterTheme, contour=T, margin=F)#, at=at.vals, colorkey=colkey)
+print(p)
+dev.off()
+
+
+# Write PNGs from part two: thresholds
+png(paste0(plotDir, "/flam_spacetime_threshDist_", threshold, "_", domain, ".png"), height=2400, width=2400, res=200)
+p <- levelplot(s, maxpixels=ncell(s), main=paste("1950-2009", threshold, "flammability threshold distributions"), par.settings=revRasterTheme, contour=T, margin=F)#, at=at.vals, colorkey=colkey)
 grid.newpage()
 pushViewport(viewport(layout=grid.layout(2, 2, widths=unit(c(1, 1), "null"), heights=unit(c(1, 1), "null"))))
 
