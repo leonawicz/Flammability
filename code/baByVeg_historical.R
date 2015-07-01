745555fc12ac04d3ec447d7bd8828bdefc35017b
Matt Leonawicz <matt.rstat@gmail.com>
2015-07-01 12:20:43 -0600

    Add masking and summarizing code

--- code/baByVeg_historical.R
+++ code/baByVeg_historical.R
@@ -1,9 +1,20 @@
 # @knitr script
 library(raster)
+library(data.table)
+library(dplyr)
 setwd("/workspace/UA/mfleonawicz/leonawicz/projects/Flammability/data")
 outDir <- "../workspaces/gbmFlammability"
+shp <- shapefile("../data/shapefiles/noa_basin2/Noa_basin2")
+
+noatak=TRUE
+noatak=FALSE
+
 b <- brick("historicalFireObs/firescarbrick_annual_observed_Statewide_lightning_1950_2013.tif")
 r.veg <- raster("alf2005.cavm.merged.030212.tif")
+if(noatak){
+    r.veg <- mask(crop(r.veg, shp), shp)
+    b <- mask(crop(b, shp), shp)
+}
 v <- r.veg[]
 ind.list <- lapply(1:7, function(i, x) which(x==i), x=v)
 names(ind.list) <- c("alp.tundra", "mariana", "glauca", "decid", "shrub", "gram", "wetland")
@@ -26,4 +37,17 @@ head(d.ba)
 apply(d.ba[,-1], 2, sum)
 apply(d.ba[,-1], 1, sum)
 
-save(d.ba, file=file.path(outDir, "baByVeg_historical_1950_2013.RData"))
+d.ba %>% mutate(total=alp.tundra + mariana + glauca + decid + shrub + gram + wetland) %>%
+    mutate(alp.tundra=alp.tundra/total,
+           marianaBApct=mariana/total,
+           glaucaBApct=glauca/total,
+           decidBApct=decid/total,
+           shrubBApct=shrub/total,
+           gramBApct=gram/total,
+           wetlandBApct=wetland/total
+           ) %>% round(3) -> d.ba.pct
+
+if(noatak) {d.ba.noatak <- d.ba; d.ba.pct.noatak <- d.ba.pct }
+if(!noatak) {d.ba.sw <- d.ba; d.ba.pct.sw <- d.ba.pct }
+
+save(d.ba.pct.sw, d.ba.pct.noatak, file=file.path(outDir, "baByVeg_historical_1950_2013.RData"))
