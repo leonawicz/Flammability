e71cdf03972bf4b36cdeb0399792487c7bec993f
Matt Leonawicz <matt.rstat@gmail.com>
2015-08-04 13:48:41 -0600

    Prep plots for knitr html context

--- code/gbm_lightning_coefficients.R
+++ code/gbm_lightning_coefficients.R
@@ -24,22 +24,25 @@ bins.cru <- get_classes2(bins.cru)
 d <- data.table(Period="historical", Model="CRU32", Year=1950:2011, LightPred=pred.light.hist, Rank=rank(pred.light.hist), Class=bins.cru, Coef=coef.cru, ECDF=ecdf(pred.light.hist)(pred.light.hist))
 
 # @knitr plots_cru32
-g1 <- ggplot(data=d, aes(x=Rank, y=LightPred, label=Year)) +
+(g1 <- ggplot(data=d, aes(x=Rank, y=LightPred, label=Year)) +
     geom_hline(yintercept=qtiles, linetype=2) + geom_point() +
     geom_text(aes(colour=Class), hjust=0, vjust=0, size=3, show_guide=F) +
     annotate("text", x=1, y=qtiles, label=c(paste("qunatile =", lb), paste("quantile =", ub)), size=3, vjust=-0.5) +
-    labs(x="Predicted rank", y="Predicted number of strikes", title="1950-2011 GBM-predicted summer lightning strikes: ranked and ordered")
-g2 <- ggplot(data=d, aes(x=Year, y=LightPred, label=Rank)) +
+    labs(x="Predicted rank", y="Predicted number of strikes", title="1950-2011 GBM-predicted summer lightning strikes: ranked and ordered"))
+    
+(g2 <- ggplot(data=d, aes(x=Year, y=LightPred, label=Rank)) +
     geom_line() + geom_hline(yintercept=qtiles, linetype=2) + geom_point() +
     geom_text(aes(colour=Class), hjust=0, vjust=0, size=3, show_guide=F) +
     annotate("text", x=1950, y=qtiles, label=c(paste("qunatile =", lb), paste("quantile =", ub)), size=3, vjust=-0.5) +
-    labs(x="Year", y="Predicted number of strikes", title="1950-2011 GBM-predicted summer lightning strikes: time series")
-g3 <- ggplot(data=d, aes(x=LightPred, label=Year)) +
+    labs(x="Year", y="Predicted number of strikes", title="1950-2011 GBM-predicted summer lightning strikes: time series"))
+    
+(g3 <- ggplot(data=d, aes(x=LightPred, label=Year)) +
     geom_vline(xintercept=qtiles, linetype=2) + stat_ecdf() + stat_ecdf(geom="point") +
     geom_text(aes(y=ECDF, colour=Class), hjust=0, vjust=0, size=3, show_guide=F) +
     annotate("text", x=qtiles, y=0, label=c(paste("qunatile =", lb), paste("quantile =", ub)), size=3, hjust=-0.1) +
-    labs(x="Predicted number of strikes", y="CDF", title="1950-2011 GBM-predicted summer lightning strikes: empirical CDF")
+    labs(x="Predicted number of strikes", y="CDF", title="1950-2011 GBM-predicted summer lightning strikes: empirical CDF"))
 
+# @knitr notrun1
 png(file.path(plotDir, "gbm_pred_1950_2011_Rank.png"), width=3200, height=1600, res=200)
 g1
 dev.off()
@@ -64,25 +67,28 @@ d2 %>% group_by(Model) %>%
 
 d.all <- rbind(d, d2)
 
-g4 <- ggplot(data=d.all, aes(x=Rank, y=LightPred, label=Year)) +
+(g4 <- ggplot(data=d.all, aes(x=Rank, y=LightPred, label=Year)) +
     geom_hline(yintercept=qtiles, linetype=2) + geom_point() +
     geom_text(aes(colour=Class), hjust=0, vjust=0, size=3, show_guide=F) +
     annotate("text", x=1, y=qtiles, label=c(paste("CRU32\nqunatile =", lb), paste("CRU32\nquantile =", ub)), size=3, vjust=-0.5) +
     labs(x="Predicted rank", y="Predicted number of strikes", title="1950-2011 CRU32 and 2010-2099 GCM GBM-predicted summer lightning strikes: ranked and ordered") +
-    facet_wrap(~ Model, ncol=2)
-g5 <- ggplot(data=d.all, aes(x=Year, y=LightPred, label=Rank)) +
+    facet_wrap(~ Model, ncol=2))
+    
+(g5 <- ggplot(data=d.all, aes(x=Year, y=LightPred, label=Rank)) +
     geom_line() + geom_hline(yintercept=qtiles, linetype=2) + geom_point() +
     geom_text(aes(colour=Class), hjust=0, vjust=0, size=3, show_guide=F) +
     annotate("text", x=1950, y=qtiles, label=c(paste("CRU32\nqunatile =", lb), paste("CRU32\nquantile =", ub)), size=3, vjust=-0.5) +
     labs(x="Year", y="Predicted number of strikes", title="1950-2011 CRU32 and 2010-2099 GCM GBM-predicted summer lightning strikes: time series") +
-    facet_wrap(~ Model, ncol=2)
-g6 <- ggplot(data=d.all, aes(x=LightPred, label=Year)) +
+    facet_wrap(~ Model, ncol=2))
+    
+(g6 <- ggplot(data=d.all, aes(x=LightPred, label=Year)) +
     geom_vline(xintercept=qtiles, linetype=2) + stat_ecdf() + stat_ecdf(geom="point") +
     geom_text(aes(y=ECDF, colour=Class), hjust=0, vjust=0, size=3, show_guide=F) +
     annotate("text", x=qtiles, y=0, label=c(paste("CRU32\nqunatile =", lb), paste("CRU32\nquantile =", ub)), size=3, hjust=-0.1, vjust=-0.25) +
     labs(x="Predicted number of strikes", y="CDF", title="1950-2011 CRU32 and 2010-2099 GCM GBM-predicted summer lightning strikes: empirical CDF") +
-    facet_wrap(~ Model, ncol=2)
-
+    facet_wrap(~ Model, ncol=2))
+    
+# @knitr notrun1
 png(file.path(plotDir, "gbm_pred_1950_2099_RankbyModel.png"), width=3200, height=1600, res=200)
 g4
 dev.off()
