af92174942544c08849c6d462245d786c4c8ed1e
Matt Leonawicz <matt.rstat@gmail.com>
2015-09-08 11:04:48 -0600

    Update data outputs

--- code/baByVeg_historical.R
+++ code/baByVeg_historical.R
@@ -9,29 +9,36 @@ shp <- shapefile("../data/shapefiles/noa_basin2/Noa_basin2")
 noatak=TRUE
 noatak=FALSE
 
-b <- brick("historicalFireObs/firescarbrick_annual_observed_Statewide_lightning_1950_2013.tif")
 r.veg <- raster("alf2005.cavm.merged.030212.tif")
+r.veg[r.veg==0] <- NA
 if(noatak){
+    b <- brick("historicalFireObs/fireIDbrick_annual_observed_Noatak_lightning_1950_2013.tif")
     r.veg <- mask(crop(r.veg, shp), shp)
     b <- mask(crop(b, shp), shp)
-}
+} else b <- brick("historicalFireObs/fireIDbrick_annual_observed_Statewide_lightning_1950_2013.tif")
+b <- mask(b, r.veg)
 v <- r.veg[]
 ind.list <- lapply(1:7, function(i, x) which(x==i), x=v)
 names(ind.list) <- c("alp.tundra", "mariana", "glauca", "decid", "shrub", "gram", "wetland")
 yrs <- 1950:2013
 
-getVegFire <- function(b, v){
+getVegFire <- function(b, v, yrs){
+    d.fs.list <- vector("list", nlayers(b))
     for(i in 1:nlayers(b)){
-        fire.ind <- which(subset(b, i)[]==1)
+        r <- subset(b, i)   
+        data.table(Year=yrs[i], freq(r)) %>% setnames(c("Year", "FID", "FS")) %>% filter(!is.na(FID)) -> d.fs.list[[i]]
+        fire.ind <- which(r[]>0)
         x <- sapply(v, function(i, x) length(intersect(x, i)), x=fire.ind)
         if(i==1) ba <- x else ba <- rbind(ba, x)
         print(nlayers(b)-i)
     }
     rownames(ba) <- NULL
-    data.frame(Year=yrs, ba)
+    list(ba=data.frame(Year=yrs, ba), fs=rbindlist(d.fs.list))
 }
 
-d.ba <- getVegFire(b, ind.list)
+d <- getVegFire(b, ind.list, yrs)
+d.ba <- d$ba
+d.fs <- d$fs
 
 head(d.ba)
 apply(d.ba[,-1], 2, sum)
@@ -47,7 +54,14 @@ d.ba %>% mutate(total=alp.tundra + mariana + glauca + decid + shrub + gram + wet
            wetlandBApct=wetland/total
            ) %>% round(3) -> d.ba.pct
 
-if(noatak) {d.ba.noatak <- d.ba; d.ba.pct.noatak <- d.ba.pct }
-if(!noatak) {d.ba.sw <- d.ba; d.ba.pct.sw <- d.ba.pct }
+if(noatak) {
+    d.ba.noatak <- d.ba
+    d.ba.pct.noatak <- d.ba.pct
+    d.fs.noatak <- d.fs
+} else {
+    d.ba.sw <- d.ba
+    d.ba.pct.sw <- d.ba.pct
+    d.fs.sw <- d.fs
+}
 
-save(d.ba.pct.sw, d.ba.pct.noatak, file=file.path(outDir, "baByVeg_historical_1950_2013.RData"))
+save(d.ba.pct.sw, d.ba.pct.noatak, d.fs.sw, d.fs.noatak, file=file.path(outDir, "baByVeg_historical_1950_2013.RData"))
