c33c81eff2b948af9aaa2784272d8611bd2b7644
leonawicz <matt.rstat@gmail.com>
2017-07-27 09:50:32 -0600

    Add intial FMO options and command line arguments.

--- code/alfresco/make_sensitivity_ignition_maps.R
+++ code/alfresco/make_sensitivity_ignition_maps.R
@@ -2,8 +2,12 @@ library(raster)
 setwd("/big_scratch/mfleonawicz/Alf_Files_20121129")
 
 comArgs <- commandArgs(trailingOnly=TRUE)
-outDir <- comArgs[3]
-domain <- comArgs[4]
+ig_fmo <- comArgs[3]
+fs_fmo <- comArgs[4]
+ig_fmo_max <- comArgs[5]
+fs_fmo_max <- comArgs[6]
+outDir <- comArgs[7]
+domain <- comArgs[8]
 
 if(substr(domain, 1, 6)=="Statew"){
   msk <- readAll(raster("alf2005.cavm.merged.030212.tif"))
@@ -11,9 +15,22 @@ if(substr(domain, 1, 6)=="Statew"){
   msk <- readAll(raster("binary_ignition.tif"))
 }
 
+apply_fmo <- function(r, mapid, fmo, fmo_max, domain){
+  if(fmo=="None") return(r)
+  if(fmo=="Refuges"){
+    x <- readAll(raster(paste0("fmo_", mapid, "_refuges.tif")))
+    if(substr(domain, 1, 6)=="Noatak") x <- mask(x, crop(x, r))
+    x <- 1 - fmo_max * x / max(x[], na.rm=TRUE)
+    return(x*r)
+  }
+  return(r)
+}
+
 ignition <- msk
 ignition[ignition > 0] <- as.numeric(comArgs[1])
+ignition <- apply_fmo(ignition, "ig", ig_fmo, ig_fmo_max, domain)
 ignition[is.na(ignition)] <- 0
+
 writeRaster(ignition, filename=file.path(outDir, 'ignition.tif'), options='COMPRESS=LZW', overwrite=TRUE, datatype='FLT4S')
 
 sensitivity <- msk
@@ -24,7 +41,7 @@ if(substr(domain, 1, 6)=="Noatak"){
   dem <- resample(raster("AKCanada_2km_DEM_mosaic.tif"), msk)
   sensitivity[dem > 750] <- 0
 }
-
 sensitivity[sensitivity > 0] <- as.numeric(comArgs[2])
+sensitivity <- apply_fmo(sensitivity, "fs", fs_fmo, fs_fmo_max, domain)
 sensitivity[is.na(sensitivity)] <- 0
 writeRaster(sensitivity, filename=file.path(outDir, 'sensitivity.tif'), options='COMPRESS=LZW', overwrite=TRUE, datatype='FLT4S')
