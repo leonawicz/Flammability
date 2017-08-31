737537f43ea5716e0d1369f6c7c0857c6351fd8e
leonawicz <matt.rstat@gmail.com>
2017-08-31 07:10:34 -0600

    Add owner.

--- code/alfresco/alf_gcmRuns_inputPrep.R
+++ code/alfresco/alf_gcmRuns_inputPrep.R
@@ -2,5 +2,6 @@
 library(alfresco)
 domain <- "Statewide" # "Noatak" # Two options for domains.
 run.name <- "fmo00s00i_historical_CRU32"
-outputs_as_inputs(domain, run.name, 1949)
-outputs_as_inputs(domain, run.name, 2013)
+owner <- "paul.duffy@neptuneinc.org"
+outputs_as_inputs(domain, run.name, 1949, owner)
+outputs_as_inputs(domain, run.name, 2013, owner)
